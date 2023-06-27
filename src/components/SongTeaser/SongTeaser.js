b8867685b15ac3be0e5f74438fb595d5c687c4e1
Filiph Eriksson Falk <filiph.erikssonfalk@gmail.com>
2023-06-27 23:31:52 +0200

    Fixed the styling of SongTeasers where the images were too small

--- src/components/SongTeaser/SongTeaser.js
+++ src/components/SongTeaser/SongTeaser.js
@@ -95,6 +95,8 @@ const SongTeaser = ({ song, className }) => {
         {song.song.coverArt && (
           <Image
             {...song.song.coverArt}
+            width={300}
+            height={300}
             src={song.song.coverArt.sourceUrl}
             dangerouslySetInnerHTML={song.song.coverArt.caption}
           />
